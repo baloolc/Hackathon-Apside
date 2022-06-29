f1cd72bf1de3d179a1cd406dcbb8b898655f49a5
baloolc <viann45@hotmail.fr>
2022-06-29 21:53:13 +0200

    upload img +view enterprise

--- src/Entity/Enterprise.php
+++ src/Entity/Enterprise.php
@@ -4,17 +4,45 @@ namespace App\Entity;
 
 use App\Repository\EnterpriseRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Symfony\Component\HttpFoundation\File\File;
 
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: EnterpriseRepository::class)]
 class Enterprise
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+    private int $id;
 
+    #[Assert\Length(
+        max: 255,
+    )]
+    #[Assert\NotBlank]
     #[ORM\Column(type: 'string', length: 255)]
-    private $name;
+    private string $name;
+
+    #[Vich\UploadableField(mapping: 'enterprise_image', fileNameProperty: 'photo')]
+    #[Assert\File(
+        maxSize: '1M',
+        mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],
+    )]
+    private ?File $photoFile = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $photo;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?DateTimeInterface $updatedAt = null;
+
+    public function __construct()
+    {
+        $this->updatedAt = new DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -31,5 +59,33 @@ class Enterprise
         $this->name = $name;
 
         return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function setPhotoFile(?File $photoFile = null): void
+    {
+        $this->photoFile = $photoFile;
+
+        if (null !== $photoFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new DateTimeImmutable();
+        }
+    }
+
+    public function getPhotoFile(): ?File
+    {
+        return $this->photoFile;
     }
 }
